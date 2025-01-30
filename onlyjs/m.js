4f0113aac44dbc4290d7fb03f96300fa783c9599
Arsalan Khan (i24k3) <akha3743@gmail.com>
2025-01-30 21:40:07 +0000

    found and fixed bug in responsive method, still needs update

--- onlyjs/m.js
+++ onlyjs/m.js
@@ -2,7 +2,26 @@ export function create(e,t=""){if(typeof e==="object"){const o=document.createEl
     The html tag/element you are trying to create dosn't exist: use a valid name.
     `;console.warn(i)}if(t!==""){for(const a in t){if(t.hasOwnProperty(a)){o.style[a]=t[a]}}}if(t.resp){applyResponsiveStyles(o,t.resp)}window.addEventListener("resize",()=>{if(t.resp){applyResponsiveStyles(o,t.resp)}});return o}else{console.warn(`
   method 'create()' takes object's as argument,
-  i.e 'create(elementObj, elementStyleObj)', please provide parms of suitable data type`)}}export function applyResponsiveStyles(e,t){const o=window.innerWidth;if(o<=600&&t.small){Object.assign(e.style,t.small)}else if(o<=1024&&t.medium){Object.assign(e.style,t.medium)}else if(e=>1024&&t.large){Object.assign(e.style,t.large)}}export function addEvent(e,t,o){if(e&&typeof t==="string"&&typeof o==="function"){e.addEventListener(t,o)}else{console.warn("Invalid arguments passed to addEvent().")}}export function setEvent(e,t){if(this&&typeof e==="string"&&typeof t==="function"){this.addEventListener(e,t)}else{console.warn("Invalid arguments passed to setEvent().")}}Object.defineProperty(HTMLElement.prototype,"t",{value:function(e,t){setEvent.call(this,e,t)},writable:true,o:true});export function onlyjs(e){if(document.readyState==="loading"){document.addEventListener("DOMContentLoaded",e)}else{e()}}(function(){if(typeof main==="function"){onlyjs(main)}else{console.log("welcome to onlyjs, The palce where you only need JS")}})();export function render(e=document.body,t,o){const n=["after","inside"];const s=t.toLowerCase();if(!n.includes(s)){console.warn(`Incorrect param passed to method render():
+  i.e 'create(elementObj, elementStyleObj)', please provide parms of suitable data type`)}}
+ 
+
+
+export function applyResponsiveStyles(e,t){const o=window.innerWidth;
+
+
+
+if (o <= 600 && t.small && Object.keys(t.small).length > 0) {
+        Object.assign(e.style, t.small);
+    } else if (o <= 1024 && t.medium && Object.keys(t.medium).length > 0) {
+        Object.assign(e.style, t.medium);
+    } else if (o > 1024 && t.large && Object.keys(t.large).length > 0) {
+        Object.assign(e.style, t.large);
+    }
+}
+
+
+
+  export function addEvent(e,t,o){if(e&&typeof t==="string"&&typeof o==="function"){e.addEventListener(t,o)}else{console.warn("Invalid arguments passed to addEvent().")}}export function setEvent(e,t){if(this&&typeof e==="string"&&typeof t==="function"){this.addEventListener(e,t)}else{console.warn("Invalid arguments passed to setEvent().")}}Object.defineProperty(HTMLElement.prototype,"t",{value:function(e,t){setEvent.call(this,e,t)},writable:true,o:true});export function onlyjs(e){if(document.readyState==="loading"){document.addEventListener("DOMContentLoaded",e)}else{e()}}(function(){if(typeof main==="function"){onlyjs(main)}else{console.log("welcome to onlyjs, The palce where you only need JS")}})();export function render(e=document.body,t,o){const n=["after","inside"];const s=t.toLowerCase();if(!n.includes(s)){console.warn(`Incorrect param passed to method render():
 whereInDom : string =  'after' or 'inside'
 By default the position set to 'document.body.<thisElement>'`);return}const i=e instanceof NodeList?e[0]:e;switch(s){case n[0]:i.insertAdjacentElement("afterend",o);break;case n[1]:i.appendChild(o);break}}export function clear(){const e={transition:"opacity 2s ease-out",opacity:"0"};document.body.i(e);setTimeout(()=>{document.body.innerHTML=""},2e3)}export function resetStyles(){var e=document.createElement("style");e.textContent=`
   * {
@@ -14,6 +33,6 @@ By default the position set to 'document.body.<thisElement>'`);return}const i=e 
     font-family: Arial, sans-serif;
   }
   /* Add more styles as needed */
-`;document.head.appendChild(e)}export function select(e){const t=document.querySelectorAll(e);return t}function n(e){if(e instanceof NodeList){return e}if(e instanceof Element){return[e]}if(typeof e==="string"){try{const o=document.querySelectorAll(e);if(o){return o}else{console.log("The selector (id, class, node) string-name is invalid, no element found!")}}catch(t){console.error("Invalid selector string:",t)}}}export function style(e,t){e=n(e);if(typeof t==="object"&&t!==null){for(const o in t){if(t.hasOwnProperty(o)){e.forEach(e=>{e.style[o]=t[o]})}}if(t.resp){applyResponsiveStyles(htmlElement,t.resp)}}else{console.warn(`
+`;document.head.appendChild(e)}export function select(e){const t=document.querySelectorAll(e);return t}function n(e){if(e instanceof NodeList){return e}if(e instanceof Element){return[e]}if(typeof e==="string"){try{const o=document.querySelectorAll(e);if(o){return o}else{console.log("The selector (id, class, node) string-name is invalid, no element found!")}}catch(t){console.error("Invalid selector string:",t)}}}export function style(e,t){e=n(e);if(typeof t==="object"&&t!==null){for(const o in t){if(t.hasOwnProperty(o)){e.forEach(e=>{e.style[o]=t[o]})}}if(t.resp){applyResponsiveStyles(e,t.resp)}}else{console.warn(`
     The style method takes objects as arguements,
     'style(elementObj, styleObj);' please provide param of suitable datatype`)}}export function setStyle(t){if(typeof t==="object"&&t!==null){if(this instanceof NodeList||this instanceof HTMLCollection){this.forEach(function(e){e.i(t)})}else{this.style.cssText="";for(const e in t){if(t.hasOwnProperty(e)){this.style[e]=t[e]}}if(t.resp){applyResponsiveStyles(this,t.resp)}}}else{console.warn("The setStyle method expects an object as an argument.")}}Object.defineProperty(HTMLElement.prototype,"i",{value:function(e){setStyle.call(this,e)},writable:true,o:true});Object.defineProperty(NodeList.prototype,"i",{value:function(e){setStyle.call(this,e)},writable:true,o:true});
