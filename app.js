a25a54c993d2b9e6024f3daea33ecedfd23b15a6
Arsalan Khan (i24k3) <akha3743@gmail.com>
2025-01-30 22:20:50 +0000

    building in progress

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ import { create, render, select } from "./onlyjs/m.js";
 import { style, setStyle } from "./onlyjs/m.js";
 
 import { header } from "./src/header.js";
+import { content } from "./src/content.js";
 
 onlyjs(main);
 function main() {
   resetStyles();
   setTitle('Devloper of all Trades');
+
   header();
+  content();
  }
 
 
